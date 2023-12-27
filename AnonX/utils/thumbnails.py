55df3b31997cec7086619602e6ef2d6b9f38659b
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-27 10:10:13 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -9,7 +9,6 @@ import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
-from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 from AnonX import app
 from config import YOUTUBE_IMG_URL
