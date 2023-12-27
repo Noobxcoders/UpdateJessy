dace440f8a54ae7aaa92db85623686cf3d8ee471
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-27 12:12:05 +0530

    Update skip.py

--- AnonX/plugins/skip.py
+++ AnonX/plugins/skip.py
@@ -127,7 +127,7 @@ async def skip(cli, message: Message, _, chat_id):
             return await message.reply_text(_["call_9"])
 
         button = telegram_markup(_, chat_id)
-        img = await gen_thumb(videoid, user_id)
+        img = await gen_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -160,7 +160,7 @@ async def skip(cli, message: Message, _, chat_id):
         except Exception:
             return await mystic.edit_text(_["call_9"])
         button = stream_markup(_, videoid, chat_id)
-        img = await gen_thumb(videoid, user_id)
+        img = await gen_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
